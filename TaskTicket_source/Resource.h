55252a5ff42e3c85417c2f90a169ccd5b638878a
sonic-me <notpradeep.pro@gmail.com>
2026-08-29 01:06:28 +0530

    Delete TaskTicket_source directory

--- TaskTicket_source/Resource.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Icon resource (optional - place TaskTicket.ico next to the .rc file and
-// uncomment the ICON line in TaskTicket.rc to use it).
-#define IDI_APPICON     101
-
-// The manifest resource id that declares Per-Monitor-V2 DPI awareness.
-// (Referenced directly by numeric id 1 / RT_MANIFEST in the .rc file.)
